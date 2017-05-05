c9816ad7777c77fecc80c3870b7f6f819647031d
Frank <frank2williams@yahoo.com>
2017-05-05 10:39:14 -0400

    CastleDefenseBoard

---
+++ src/game/CastleDefenseBoard.java
@@ -0,0 +1,12 @@
+package game;
+
+import base.Board;
+
+/**
+ * Created by Frank Williams on 5/5/2017.
+ */
+public class CastleDefenseBoard extends Board {
+    public CastleDefenseBoard (double inputWidth, double inputHeight){
+        super(inputWidth, inputHeight);
+    }
+}
