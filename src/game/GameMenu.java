c2be53321071bd1de480839eac98ad035365a73a
Senarii <Vortekdynamic@gmail.com>
2017-05-05 10:51:57 -0400

    Added Basic Buttons To Be Implemented Later

---
+++ src/game/GameMenu.java
@@ -0,0 +1,15 @@
+package game;
+
+import javax.swing.*;
+
+/**
+ * Created by Cody on 5/5/2017.
+ */
+public class GameMenu extends JPanel {
+
+
+    public GameMenu()
+    {
+
+    }
+}
--- src/game/Menu.java
+++
@@ -1,8 +0,0 @@
-package game;
-
-/**
- * Created by Cody on 5/5/2017.
- */
-public class Menu {
-
-}
