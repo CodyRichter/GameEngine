5c0a2c08763196be6c2f8d2cef05cf29bac2901d
Frank <frank2williams@yahoo.com>
2017-05-08 10:54:05 -0400

    Started game logic added basic enemy

---
+++ src/game/CastleDefense.java
@@ -0,0 +1,12 @@
+package game;
+
+/**
+ * Created by Frank Williams on 5/8/2017.
+ */
+public class CastleDefense {
+    private static int wave;
+
+    public static void nextWave(){
+        wave++;
+    }
+}
---
+++ src/game/enemies/BasicEnemy.java
@@ -0,0 +1,22 @@
+package game.enemies;
+
+import game.Enemy;
+
+/**
+ * Created by Frank Williams on 5/8/2017.
+ */
+public class BasicEnemy extends Enemy {
+    public BasicEnemy(){
+        super();
+    }
+
+    @Override
+    public void attack() {
+
+    }
+
+    @Override
+    public void onDeath() {
+
+    }
+}
