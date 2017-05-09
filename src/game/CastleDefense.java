b2e2dbe0ee949f9935ac43344ae324c595fbc489
Frank <frank2williams@yahoo.com>
2017-05-08 20:57:35 -0400

    Add some logic about adding enemies after new waves

--- src/game/CastleDefense.java
+++ src/game/CastleDefense.java
@@ -8,15 +8,20 @@ import java.util.ArrayList;
  * Created by Frank Williams on 5/8/2017.
  */
 public class CastleDefense {
-    private static int wave;
+    private static int wave = 0;
     private static ArrayList<Enemy> enemies = new ArrayList<Enemy>();
 
     public static void nextWave(){
         wave++;
-    }
-    public static void addEnemy(String type){
-        if (type == "basic"){
-            enemies.add(new BasicEnemy());
+        if(wave <= 5) {
+            for (int i = 0; i < 10; i++) {
+                Enemy enemyToAdd = new BasicEnemy();
+                Main.b.addUnit(enemyToAdd);
+                addEnemy(enemyToAdd);
+            }
         }
+    }
+    public static void addEnemy(Enemy e){
+        enemies.add(new BasicEnemy());
     }
 }
--- src/game/CastleDefenseBoard.java
+++ src/game/CastleDefenseBoard.java
@@ -1,6 +1,7 @@
 package game;
 
 import base.Board;
+import base.Unit;
 
 /**
  * Created by Frank Williams on 5/5/2017.
@@ -11,5 +12,15 @@ public class CastleDefenseBoard extends Board {
     public CastleDefenseBoard (){
         int arbitraryInt = 3; //should the menu size be a double?
         menu = new GameMenu(arbitraryInt); //should the menu size be a double?
+    }
+
+    @Override
+    public void addUnit(Unit u) {
+        super.addUnit(u);
+        if(u instanceof Enemy){
+            // check if it is an enemy and then add it to a random place off the board
+        } else if (u instanceof Friendly || u instanceof Peasant){
+            //check if it isnt an enemy and add it to the user's side of the board
+        }
     }
 }
