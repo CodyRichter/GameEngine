637e604cdc3dd6c3ef674293332b1ad2acb043cf
Senarii <Vortekdynamic@gmail.com>
2017-05-24 12:46:25 -0400

    Fixed healthbar showing up over projectiles

--- src/game/Main.java
+++ src/game/Main.java
@@ -26,7 +26,7 @@ public class Main
     //Debug variables
     public static final boolean DEBUG = false;
     public static final boolean VERBOSE = false;
-    public static final boolean ADMINMODE = false;
+    public static final boolean ADMINMODE = true;
 
     public static void main(String[] args)
     {
