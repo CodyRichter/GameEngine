f47d5c1901e57cbae94d083dcb67d3262e2425a7
Senarii <Vortekdynamic@gmail.com>
2017-05-30 10:54:20 -0400

    Fixed projectiles causing game over

--- src/game/Friendly.java
+++ src/game/Friendly.java
@@ -7,16 +7,17 @@ import game.friendly.Knight;
 import game.friendly.Militia;
 
 /**
- * Methods For game.Friendly Units
+ * Methods For Friendly Units
  *
  * @author Cody Richter
  * @version 1.0
  */
 public abstract class Friendly extends Unit
 {
+
     /**
-     * Makes New game.Friendly base.Unit
-     */
+     * Creates a new Friendly Unit with default attributes.
+      */
     public Friendly()
     {
         super(1,1,1,1,null,null);
@@ -24,7 +25,12 @@ public abstract class Friendly extends Unit
     }
 
     /**
-     * Makes new friendly unit with given attributes
+     * Creates a new Friendly unit with a set amount of hitpoints, damage, range, and speed.
+     * However, the unit sprite must be set separately with the setSprite() method.
+     * @param healthLevel
+     * @param damageLevel
+     * @param range
+     * @param speed
      */
     public Friendly(int healthLevel, int damageLevel, int range, int speed)
     {
@@ -32,17 +38,22 @@ public abstract class Friendly extends Unit
         isEnemy = false;
     }
 
-    /*
-    Moves Enemy Unit From Left Side Of Screen To The Right Relative To Movement Speed
-    */
+    /**
+     * Moves Friendly Unit from the left side of the screen to the
+     * right side of the screen. The unit's overall speed is set
+     * relative to its movement speed, and the set scaling period
+     * which maintains a constant time for the unit to cross the
+     * board depending on the size of the screen used.
+     */
     public void move()
     {
         pos.setLocation(pos.getX() - moveSpeed ,  pos.getY());
     }
 
 
-    /*
-    Spawns Unit Into Game Onto Given Row
+    /**
+     * Spawns a Friendly Unit onto the board in a given row
+     * @param row Value Ranging from 1-3 to represent row unit is spawned on.
      */
     public void spawn(int row){
         currentRow = row;
@@ -65,8 +76,9 @@ public abstract class Friendly extends Unit
         pos.setLocation(x, y);
     }
 
-    /*
-    Kills A Friendly and Removes It From The Board
+    /**
+     * Kills a Friendly Unit and removes it
+     * from all ArrayLists and containers.
      */
     public void kill()
     {
@@ -77,6 +89,11 @@ public abstract class Friendly extends Unit
         Main.menu.repaint();
     }
 
+    /**
+     * Returns The Cost Of a Given Type Of Unit.
+     * @param f Name Of Friendly Unit, As a String
+     * @return Price To Spawn Unit In
+     */
     public static int getUnitCost(Friendly f)
     {
         if (f instanceof Militia)
